d51daf44572552dcec461a960fa4146d6d4b7583
clinturbin <clinturbin@gmail.com>
2019-04-17 13:29:53 -0400

    Added event listener for Hold button

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, winningScore;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-
-document.querySelector('.dice').style.display = 'none';
+activePlayer = 0;
+winningScore = 20;
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
+let hideDice = () => {
+    document.querySelector('.dice').style.display = 'none';
+};
+
+let clearCurrentScore = () => {
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+};
+
+let switchPlayer = () => {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    clearCurrentScore();
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+};
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
     // 1. Random number
@@ -27,14 +44,28 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        document.querySelector('.dice').style.display = 'none';
+        switchPlayer();
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    // 1. Add current score to users global score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the User Interface
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // 3. Check if player won the game
+    if(scores[activePlayer] >= winningScore) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        hideDice();
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
+    } else {
+        // Switch Player
+        switchPlayer();
+    }
+});
+
+hideDice();
+clearCurrentScore();
