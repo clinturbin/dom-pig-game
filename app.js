7a1e76c1067d0232b346f6ca2dc6fd6500081a01
clinturbin <clinturbin@gmail.com>
2019-04-22 12:36:20 -0400

    Added input to allow player to set winning score

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let scores, roundScore, activePlayer, winningScore, gamePlaying, lastDice;
+let scores, roundScore, activePlayer, gamePlaying, lastDice;
 
-winningScore = 20;
 gamePlaying = true;
 
 let resetGame = () => {
@@ -68,7 +67,12 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
         diceDOM.src = 'assets/dice-' + dice + '.png';
         
         // 3. Update the round score IF the rolled number was NOT a 1
-        if (dice !== 1) {
+        if (dice === 6 && lastDice === 6) {
+            // Player loses score
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = '0';
+            switchPlayer();
+        } else if (dice !== 1) {
             // Add score
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -87,6 +91,16 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
 
         // 2. Update the User Interface
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        let input = document.querySelector('.final-score').value;
+        let winningScore;
+
+        // Undefined, 0, null or "" are COERCED to false
+        // anything else is COERCED to true
+        if (input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
         
         // 3. Check if player won the game
         if(scores[activePlayer] >= winningScore) {
